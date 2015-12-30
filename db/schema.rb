f38343a352066b1318eb8fec4d64ca8676f79677
王信凱 <jcskyting@gmail.com>
2015-12-31 01:00:21 +0800

    add identity model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230145457) do
+ActiveRecord::Schema.define(version: 20151230165109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,17 @@ ActiveRecord::Schema.define(version: 20151230145457) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "identities", force: :cascade do |t|
+    t.string   "uid"
+    t.string   "provider"
+    t.string   "token"
+    t.uuid     "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
 
   create_table "parties", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.string   "name"
