d4cc8a6e70df09c2bf90802a60939b012e763d12
王信凱 <jcskyting@gmail.com>
2015-12-31 02:46:54 +0800

    test file(not important)

--- test/models/identity_test.rb
+++ test/models/identity_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: identities
+#
+#  id         :integer          not null, primary key
+#  uid        :string
+#  provider   :string
+#  token      :string
+#  user_id    :uuid
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class IdentityTest < ActiveSupport::TestCase
